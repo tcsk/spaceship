882efb99508064bd4b01f4d6d883cc2572a2c820
tcsk <tibornagy77@gmail.com>
2019-05-02 17:53:56 +0200

    node.js configured

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -1,6 +1,15 @@
-<?php
-phpinfo();
-
-$a = 'b';
-
-echo $a;
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Kígyós játék</title>
+    <!--<script src="js/dist/phaser-arcade-physics.min.js"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!--https://gamedevacademy.org/create-a-basic-multiplayer-game-in-phaser-3-with-socket-io-part-1/-->
+<script src="http://192.168.0.169:49999/socket.io/socket.io.js"></script>
+<script src="js/game.js"></script>
+</body>
+</html>
